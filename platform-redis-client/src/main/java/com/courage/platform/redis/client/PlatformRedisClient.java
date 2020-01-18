991e33658cec905d6febd9b75b3668b5215fb4ac
zhangyong <zhangyong7120180@163.com>
2020-01-18 20:15:22 +0800

    redis client 信息

---
+++ platform-redis-client/src/main/java/com/courage/platform/redis/client/PlatformRedisClient.java
@@ -0,0 +1,15 @@
+package com.courage.platform.redis.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 平台redis 客户端
+ * Created by zhangyong on 2020/1/18.
+ */
+public class PlatformRedisClient {
+
+    private final static Logger logger = LoggerFactory.getLogger(PlatformRedisClient.class);
+
+
+}
