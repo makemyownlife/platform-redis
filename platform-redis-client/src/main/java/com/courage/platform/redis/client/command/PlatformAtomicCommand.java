6a370466864b721482e45ff60be9c64daa31ab34
zhangyong <zhangyong7120180@163.com>
2020-01-19 20:18:18 +0800

    paltform atomic command

---
+++ platform-redis-client/src/main/java/com/courage/platform/redis/client/command/PlatformAtomicCommand.java
@@ -0,0 +1,21 @@
+package com.courage.platform.redis.client.command;
+
+public interface PlatformAtomicCommand extends PlatformRedisCommand {
+
+    Long incr(String key);
+
+    Long incrBy(String key, long incValue);
+
+    Long decr(String key);
+
+    Long decrBy(String key, long incValue);
+
+    Long incrEx(String key, int seconds);
+
+    Long incrByEx(String key, long incValue, int seconds);
+
+    Long decrEx(String key, int seconds);
+
+    Long decrByEx(String key, long incValue, int seconds);
+
+}
