4f9536dfb423292e1afaa997450de2fa49425267
zhangyong <zhangyong7120180@163.com>
2020-01-21 11:43:02 +0800

    list codec

--- platform-redis-client/src/main/java/com/courage/platform/redis/client/command/impl/PlatformListCommandImpl.java
+++ platform-redis-client/src/main/java/com/courage/platform/redis/client/command/impl/PlatformListCommandImpl.java
@@ -6,10 +6,13 @@ import com.courage.platform.redis.client.enums.PlatformRedisCommandType;
 import org.redisson.api.RBlockingDeque;
 import org.redisson.api.RList;
 import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
 
 import java.util.List;
 
 public class PlatformListCommandImpl extends PlatformKeyCommand implements PlatformListCommand {
+
+    private static JsonJacksonCodec listCodec = new JsonJacksonCodec();
 
     public PlatformListCommandImpl(RedissonClient redissonClient) {
         super(redissonClient);
@@ -18,7 +21,7 @@ public class PlatformListCommandImpl extends PlatformKeyCommand implements Platf
     public Boolean lpush(final String key, final Object o) {
         return invokeCommand(new PlatformInvokeCommand<Boolean>(PlatformRedisCommandType.LPUSH) {
             public Boolean exe(RedissonClient redissonClient) {
-                RList result = getRedissonClient().getList(key);
+                RList result = getRedissonClient().getList(key, listCodec);
                 return Boolean.valueOf(result.add(o));
             }
         });
@@ -27,7 +30,7 @@ public class PlatformListCommandImpl extends PlatformKeyCommand implements Platf
     public List lrange(final String key, final int startNum, final int endNum) {
         return invokeCommand(new PlatformInvokeCommand<List>(PlatformRedisCommandType.LRANGE) {
             public List exe(RedissonClient redissonClient) {
-                RList result = getRedissonClient().getList(key);
+                RList result = getRedissonClient().getList(key, listCodec);
                 return result.subList(startNum, endNum);
             }
         });
@@ -36,7 +39,7 @@ public class PlatformListCommandImpl extends PlatformKeyCommand implements Platf
     public void ltrim(final String key, final int startNum, final int endNum) {
         invokeCommand(new PlatformInvokeCommand<String>(PlatformRedisCommandType.LTRIM) {
             public String exe(RedissonClient redissonClient) {
-                RList result = getRedissonClient().getList(key);
+                RList result = getRedissonClient().getList(key, listCodec);
                 result.trim(startNum, endNum);
                 return null;
             }
@@ -46,7 +49,7 @@ public class PlatformListCommandImpl extends PlatformKeyCommand implements Platf
     public Integer llen(final String key) {
         return invokeCommand(new PlatformInvokeCommand<Integer>(PlatformRedisCommandType.LLEN) {
             public Integer exe(RedissonClient redissonClient) {
-                RList result = getRedissonClient().getList(key);
+                RList result = getRedissonClient().getList(key, listCodec);
                 return result.size();
             }
         });
@@ -55,7 +58,7 @@ public class PlatformListCommandImpl extends PlatformKeyCommand implements Platf
     public <T> T rpop(final String key) {
         return invokeCommand(new PlatformInvokeCommand<T>(PlatformRedisCommandType.RPOP) {
             public T exe(RedissonClient redissonClient) {
-                RBlockingDeque result = getRedissonClient().getBlockingDeque(key);
+                RBlockingDeque result = getRedissonClient().getBlockingDeque(key, listCodec);
                 return (T) result.poll();
             }
         });
@@ -64,7 +67,7 @@ public class PlatformListCommandImpl extends PlatformKeyCommand implements Platf
     public <T> T lpop(final String key) {
         return invokeCommand(new PlatformInvokeCommand<T>(PlatformRedisCommandType.LPOP) {
             public T exe(RedissonClient redissonClient) {
-                RBlockingDeque result = getRedissonClient().getBlockingDeque(key);
+                RBlockingDeque result = getRedissonClient().getBlockingDeque(key, listCodec);
                 return (T) result.pollFirst();
             }
         });
@@ -73,7 +76,7 @@ public class PlatformListCommandImpl extends PlatformKeyCommand implements Platf
     public boolean rpush(final String key, final Object o) {
         return invokeCommand(new PlatformInvokeCommand<Boolean>(PlatformRedisCommandType.RPUSH) {
             public Boolean exe(RedissonClient redissonClient) {
-                RBlockingDeque result = getRedissonClient().getBlockingDeque(key);
+                RBlockingDeque result = getRedissonClient().getBlockingDeque(key, listCodec);
                 return result.offerLast(o);
             }
         });
