f5f55f378eed95a1563e7aaafbb2bcb097a3da54
zhangyong <zhangyong7120180@163.com>
2020-01-21 20:55:27 +0800

    single server config

--- platform-redis-client-springboot-starter/src/main/java/com/courage/platform/redis/client/springboot/starter/configuration/PlatformRedisTypeConfiguration.java
+++ platform-redis-client-springboot-starter/src/main/java/com/courage/platform/redis/client/springboot/starter/configuration/PlatformRedisTypeConfiguration.java
@@ -1,6 +1,11 @@
 package com.courage.platform.redis.client.springboot.starter.configuration;
 
+import org.redisson.config.Config;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 根据type类型不同 配置不同的
@@ -9,6 +14,17 @@ import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 @AutoConfigureBefore(PlatformRedisClientConfiguration.class)
 public class PlatformRedisTypeConfiguration {
 
+    @Configuration
+    @ConditionalOnMissingBean(Config.class)
+    @ConditionalOnProperty(name = "platform.redis.type", havingValue = "SINGLE")
+    static class SingleServerConfig {
+
+        @Bean
+        public Config singleServerConfig() {
+            return null;
+        }
+
+    }
 
 
 }
