207a1c888eafb4d121ecc7811d0f361bda1ae959
zhangyong <zhangyong7120180@163.com>
2020-01-22 19:49:45 +0800

    zset command

---
+++ platform-redis-client/src/main/java/com/courage/platform/redis/client/command/PlatformZSetCommand.java
@@ -0,0 +1,34 @@
+package com.courage.platform.redis.client.command;
+
+import java.util.Map;
+import java.util.Set;
+
+public interface PlatformZSetCommand extends PlatformRedisCommand {
+
+    Long zadd(final String key, final Map<String, Double> scoreMembers);
+
+    Long zaddAndEx(final String key, final Map<String, Double> scoreMembers, int aliveSecond);
+
+    Long zadd(final String key, final double score, final String member);
+
+    Long zaddAndEx(final String key, final double score, final String member, int aliveSecond);
+
+    Set<String> zrange(final String key, final long start, final long end);
+
+    Set<String> zrangeByScore(final String key, final double min, final double max);
+
+    Long zrem(final String key, final String... members);
+
+    Long zremrangeByScore(final String key, double min, double max);
+
+    Double zincrbyAndEx(final String key, final double score, final String member, int aliveSecond);
+
+    Double zincrby(final String key, final double score, final String member);
+
+    Long zrank(final String key, final String member);
+
+    Long zrevrank(final String key, final String member);
+
+    Set<String> zrevrange(final String key, final long start, final long end);
+
+}
