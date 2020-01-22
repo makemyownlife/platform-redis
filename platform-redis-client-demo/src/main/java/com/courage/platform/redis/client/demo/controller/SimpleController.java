da2e35e59adfdb2dac9639fe9a5642969cc4b125
zhangyong <zhangyong7120180@163.com>
2020-01-22 16:45:14 +0800

    hello zk

---
+++ platform-redis-client-demo/src/main/java/com/courage/platform/redis/client/demo/controller/SimpleController.java
@@ -0,0 +1,29 @@
+package com.courage.platform.redis.client.demo.controller;
+
+import com.courage.platform.redis.client.PlatformRedisClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SimpleController {
+
+    private final static Logger logger = LoggerFactory.getLogger(SimpleController.class);
+
+    @Autowired
+    private PlatformRedisClient platformRedisClient;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hellodanbiao() {
+        String mylife = platformRedisClient.getPlatformStringCommand().get("hello");
+        System.out.println(mylife);
+        platformRedisClient.getPlatformStringCommand().set("hello", "zhang勇");
+        return "hello-service";
+    }
+
+}
