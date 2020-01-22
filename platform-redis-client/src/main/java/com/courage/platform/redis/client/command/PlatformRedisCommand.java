e4fc6d63a8f7bfd8471ee01b6b8befe7b4ccd77e
zhangyong <zhangyong7120180@163.com>
2020-01-22 11:46:38 +0800

    del exist

--- platform-redis-client/src/main/java/com/courage/platform/redis/client/command/PlatformRedisCommand.java
+++ platform-redis-client/src/main/java/com/courage/platform/redis/client/command/PlatformRedisCommand.java
@@ -2,10 +2,6 @@ package com.courage.platform.redis.client.command;
 
 public interface PlatformRedisCommand {
 
-    static final byte[] NX = "NX".getBytes();
-
-    static final byte[] EX = "EX".getBytes();
-
     boolean del(String key);
 
     boolean exists(String key);
