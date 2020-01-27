ddcc531b094833e5112c16323f798418dfd12895
zhangyong <zhangyong7120180@163.com>
2020-01-27 22:39:51 +0800

    script

--- platform-redis-client/src/main/java/com/courage/platform/redis/client/command/PlatformScriptCommand.java
+++ platform-redis-client/src/main/java/com/courage/platform/redis/client/command/PlatformScriptCommand.java
@@ -8,6 +8,10 @@ import java.util.List;
  */
 public interface PlatformScriptCommand extends PlatformKeyCommand {
 
+    /*
+       KEYS[1] 用来表示在redis 中用作键值的参数占位，主要用來传递在redis 中用作keyz值的参数。
+       ARGV[1] 用来表示在redis 中用作参数的占位，主要用来传递在redis中用做 value值的参数。
+     */
     String scriptLoad(String luaScript);
 
     Object evalSha(String key, String shaDigest, List<Object> keys, Object... values);
