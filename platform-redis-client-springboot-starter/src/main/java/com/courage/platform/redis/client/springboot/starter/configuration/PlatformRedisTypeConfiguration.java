9a276ddaeb7eca84f8638c171cf1705d82e326b3
zhangyong <zhangyong7120180@163.com>
2020-02-08 18:40:05 +0800

    platform server configuration

--- platform-redis-client-springboot-starter/src/main/java/com/courage/platform/redis/client/springboot/starter/configuration/PlatformRedisTypeConfiguration.java
+++ platform-redis-client-springboot-starter/src/main/java/com/courage/platform/redis/client/springboot/starter/configuration/PlatformRedisTypeConfiguration.java
@@ -1,12 +1,12 @@
 package com.courage.platform.redis.client.springboot.starter.configuration;
 
-import com.courage.platform.redis.client.springboot.starter.configuration.config.PlatformClusterServerConfigBoot;
-import com.courage.platform.redis.client.springboot.starter.configuration.config.PlatformSingleServerConfigBoot;
+import com.courage.platform.redis.client.config.PlatformClusterServerConfig;
+import com.courage.platform.redis.client.config.PlatformSingleServerConfig;
 import org.redisson.config.Config;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,15 +20,21 @@ public class PlatformRedisTypeConfiguration {
     @Configuration
     @ConditionalOnMissingBean(Config.class)
     @ConditionalOnProperty(name = "platform.redis.type", havingValue = "SINGLE")
-    @EnableConfigurationProperties(PlatformSingleServerConfigBoot.class)
     static class StaticBuildSingleServer {
 
+        @Bean(value = "platformSingleServerConfig")
+        @ConfigurationProperties(prefix = "platform.redis.singleserver")
+        public PlatformSingleServerConfig getPlatformSingleServerConfig() {
+            PlatformSingleServerConfig config = new PlatformSingleServerConfig();
+            return config;
+        }
+
         @Bean
-        public Config singleServerConfig(PlatformSingleServerConfigBoot platformSingleServerConfigBoot) {
+        public Config singleServerConfig(PlatformSingleServerConfig platformSingleServerConfig) {
             Config config = new Config();
-            config.useSingleServer().setAddress(platformSingleServerConfigBoot.getAddress());
-            if (platformSingleServerConfigBoot.getPassword() != null) {
-                config.useSingleServer().setPassword(platformSingleServerConfigBoot.getPassword());
+            config.useSingleServer().setAddress(platformSingleServerConfig.getAddress());
+            if (platformSingleServerConfig.getPassword() != null) {
+                config.useSingleServer().setPassword(platformSingleServerConfig.getPassword());
             }
             return config;
         }
@@ -38,13 +44,19 @@ public class PlatformRedisTypeConfiguration {
     @Configuration
     @ConditionalOnMissingBean(Config.class)
     @ConditionalOnProperty(name = "platform.redis.type", havingValue = "CLUSTER")
-    @EnableConfigurationProperties(PlatformClusterServerConfigBoot.class)
     static class StaticBuildClusterServer {
 
+        @Bean(value = "platformClusterServerConfig")
+        @ConfigurationProperties(prefix = "platform.redis.clusterserver")
+        public PlatformClusterServerConfig getPlatformClusterServerConfig() {
+            PlatformClusterServerConfig config = new PlatformClusterServerConfig();
+            return config;
+        }
+
         @Bean
-        public Config clusterServerConfig(PlatformClusterServerConfigBoot platformClusterServerConfigBoot) {
+        public Config clusterServerConfig(PlatformClusterServerConfig platformClusterServerConfig) {
             Config config = new Config();
-            for (String node : platformClusterServerConfigBoot.getNodes()) {
+            for (String node : platformClusterServerConfig.getNodes()) {
                 config.useClusterServers().addNodeAddress(node);
             }
             return config;
